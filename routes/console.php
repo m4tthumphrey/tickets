716e613dd4a0bc3a77307f6b7d8417b5a07bc2d6
Matt Humphrey <git@m4tt.co>
2026-02-20 08:50:23 +0000

    Fix cron periods

--- routes/console.php
+++ routes/console.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('tc:sync-teams')->daily();
+Schedule::command('tc:sync-teams')->hourlyAt('00:00');
 Schedule::command('tc:sync-venues')->daily();
 Schedule::command('tc:sync-competitions')->daily();
-Schedule::command('tc:sync-ticket-categories')->daily();
+Schedule::command('tc:sync-ticket-categories')->everyFifteenMinutes();
 Schedule::command('tc:sync-products')->everyFifteenMinutes();
