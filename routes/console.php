d9c397fb9bf05b30275b18ec278ac220ac540515
Matt Humphrey <git@m4tt.co>
2026-02-20 08:52:18 +0000

    Revert to hour;y

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('tc:sync-teams')->hourlyAt('00:00');
+Schedule::command('tc:sync-teams')->hourly();
 Schedule::command('tc:sync-venues')->daily();
 Schedule::command('tc:sync-competitions')->daily();
 Schedule::command('tc:sync-ticket-categories')->everyFifteenMinutes();
